7fb023d85b0f3c6b29cb7d2c2e38b549d837b4a4
Maksim Petrenko <maksim.p@smart-gamma.com>
2016-05-23 19:56:37 +0300

    https://crm.smart-gamma.com/issues/1439 =>fixed multiple enter coupons code;

--- wp-content/themes/canvas-child/js/cart.js
+++ wp-content/themes/canvas-child/js/cart.js
@@ -1,10 +1,15 @@
 jQuery(document).ready(function ($) {
-    if( ! $('tr.cart-discount').length ) {
+    if ($('.coupon').length) {
         // Make 'Apply Coupon' button blinking if coupon code is not entered
         setInterval(function () {
             $('input[name="apply_coupon"]').delay(200).fadeOut('slow').fadeIn('');
         }, 1000);
-    } else {
-        $('.coupon').html('<span>' + translation.coupon_applied + '</span>');
+        //$('.coupon').append('<br/><span>' + translation.coupon_applied + '</span><br/>');
+        if ($('.woocommerce-message').length) {
+            $('.coupon').append('<br/><span class="message">' + $('.woocommerce-message').text() + '</span><br/>');
+        }
+        if ($('.woocommerce-error').length) {
+            $('.coupon').append('<br/><span class="error">' + $('.woocommerce-error').text() + '</span><br/>');
+        }
     }
 });
