f09358502ceacc5a87e063bd8ad9eb83a4a84ba6
Maksim Petrenko <maksim.p@smart-gamma.com>
2016-05-30 15:06:05 +0300

    https://crm.smart-gamma.com/issues/1452 =>Replace READ MORE button after short description

--- wp-content/themes/canvas-child/templates/single-product/product-description.php
+++ wp-content/themes/canvas-child/templates/single-product/product-description.php
@@ -12,13 +12,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 <div id="product-description">
-	<button
-			class="button more read-more"
-			data-label-read-more="<?php _e('Read more', 'woocommerce'); ?>"
-			data-label-hide-more="<?php _e('Verbergen', 'woocommerce'); ?>" >
-			<?php _e('Read more', 'woocommerce'); ?>
-	</button>
 	<div class="full-content closed">
 		<?php the_content(); ?>
 	</div>
+	<button
+		class="button more read-more"
+		data-label-read-more="<?php _e('Read more', 'woocommerce'); ?>"
+		data-label-hide-more="<?php _e('Verbergen', 'woocommerce'); ?>" >
+		<?php _e('Read more', 'woocommerce'); ?>
+	</button>
 </div>
