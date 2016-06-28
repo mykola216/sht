0d3b18ca936c6f7ea53f9911a906e8daecb78346
Maksim Petrenko <maksim.p@smart-gamma.com>
2016-06-28 15:04:49 +0300

    https://crm.smart-gamma.com/issues/1505 =>Fixed remove product from cart (for Opera browser), bypass google analytics;

--- wp-content/mu-plugins/templates/ec-integration-end.php
+++ wp-content/mu-plugins/templates/ec-integration-end.php
@@ -1,3 +1,4 @@
+	/* remove via google analytics */
 	remove_from_cart = function(element) {
 		var cartItem = JSON.parse(element.dataset.item);
 		ga('ec:addProduct', {
@@ -9,8 +10,9 @@
 		ga('ec:setAction', 'remove');
 		ga('send', 'event', 'UX', 'click', 'remove from cart', {
 			'hitCallback': function() {
-				document.location = cartItem.href;
+				//document.location = cartItem.href;
 			}
 		});
+		document.location = cartItem.href;
 	}
 </script>
