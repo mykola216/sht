80d76601c8ba7711e5be5632fe4a6e0b28b64e68
Maksim Petrenko <maksim.p@smart-gamma.com>
2016-09-07 16:23:10 +0300

    https://crm.smart-gamma.com/issues/1683 =>Return back text to uppercase for text under home slider; =>Capitalize only first letter for button;

--- wp-content/themes/canvas-child/js/common.js
+++ wp-content/themes/canvas-child/js/common.js
@@ -1,7 +1,21 @@
 (function ($) {
     $(function () {
+        /* Widget h3 click handle*/
         $('.widget_recently_viewed_products').on('click', '> h3', null, function (e) {
             $(this).parent().toggleClass('opened');
         });
-    });
+
+        /* Capitalize first-letter for ".button" */
+        $('.button').each(function () {
+            if ($(this).is('input')) {
+                var oldVal = $(this).val();
+                var newVal = oldVal.slice(0,1).toUpperCase() + oldVal.slice(1).toLowerCase();
+                $(this).val(newVal);
+                console.log('newVal', newVal);
+            }
+            else if (!$(this).is('.wc-backward')) {
+                $(this).wrapInner('<span/>');
+            }
+        });
+    });// DOM ready
 })(jQuery);
