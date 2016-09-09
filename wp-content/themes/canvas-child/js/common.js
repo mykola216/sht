bc3247e44c1bd68367dc56c6a21a0f7b666d14cd
Maksim Petrenko <maksim.p@smart-gamma.com>
2016-09-09 14:02:57 +0300

    Modified: Replace protocol in URL from https: to http: for thumbnails in widget Product Of The Day

--- wp-content/themes/canvas-child/js/common.js
+++ wp-content/themes/canvas-child/js/common.js
@@ -17,12 +17,20 @@
             }
         });
 
-        /* Replace protocol in URL from https: to http: for thumbnails in widget Product Of The Day*/
+        /* Replace protocol in URL from https: to http: for thumbnails in widget Product Of The Day */
         $('.woocommerce_products_of_the_day').find('img.wp-post-image').each(function () {
-            var newSrc, newSrcSet;
-            newSrc = $(this).attr('src').replace('https://', 'http://');
-            newSrcSet = $(this).attr('srcset').replace('https://', 'http://');
-            $(this).attr('src', newSrc).attr('srcset', newSrcSet);
+            var src, srcset, newSrc, newSrcSet;
+            src = $(this).attr('src');
+            srcset = $(this).attr('srcset');
+
+            if (src) {
+                newSrc = src.replace('https://', 'http://');
+                $(this).attr('src', newSrc);
+            }
+            if (srcset) {
+                newSrcSet = srcset.replace('https://', 'http://');
+                $(this).attr('srcset', newSrcSet);
+            }
         });
     });// DOM ready
 })(jQuery);
