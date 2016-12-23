9dc1c845f4ea77176a684fa3fb389007b58de0cc
Maksim Petrenko <maksim.p@smart-gamma.com>
2016-12-23 14:56:04 +0200

    https://crm.smart-gamma.com/issues/1987 => Disabled 'Apply Coupon' button blinking;

--- wp-content/themes/canvas-child/js/cart.js
+++ wp-content/themes/canvas-child/js/cart.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function ($) {
     if ($('.coupon').length) {
         // Make 'Apply Coupon' button blinking if coupon code is not entered
-        setInterval(function () {
-            $('input[name="apply_coupon"]').delay(200).fadeOut('slow').fadeIn('');
-        }, 1000);
+        //setInterval(function () {
+        //    $('input[name="apply_coupon"]').delay(200).fadeOut('slow').fadeIn('');
+        //}, 1000);
         //$('.coupon').append('<br/><span>' + translation.coupon_applied + '</span><br/>');
         if ($('.woocommerce-message').length) {
             $('.coupon').append('<br/><span class="message">' + $('.woocommerce-message').text() + '</span><br/>');
