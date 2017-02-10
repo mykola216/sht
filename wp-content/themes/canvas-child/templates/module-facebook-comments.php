f87c45afe49965728f9853a04f19ce7afab790f3
Maksim Petrenko <maksim.p@smart-gamma.com>
2017-02-10 12:53:57 +0200

    Hot fix https to http for sharethis liks and FB comments;

--- wp-content/themes/canvas-child/templates/module-facebook-comments.php
+++ wp-content/themes/canvas-child/templates/module-facebook-comments.php
@@ -1,15 +1,12 @@
 <?php defined( 'ABSPATH' ) || die();
 /**
- * Facebook comments module.
- *
- * @author   Aleksandr Levashov
- * @version  1.0.0
- */
-
+* Facebook comments module.
+*
+* @author   Aleksandr Levashov
+* @version  1.0.0
+*/
 global $st_options;
-
 $fb_comments_number = esc_attr((int)$st_options['fb_comments_number']);
 $fb_comments_number = $fb_comments_number ? $fb_comments_number : 2;
-
 ?>
-<div class="fb-comments" data-href="<?php echo esc_attr( get_permalink() ); ?>" data-numposts="<?php echo $fb_comments_number; ?>" data-colorscheme="light" data-width="100%"></div>
+<div class="fb-comments" data-href="<?php echo esc_attr( str_replace( 'https://', 'http://', get_permalink() ) ); ?>" data-numposts="<?php echo $fb_comments_number; ?>" data-colorscheme="light" data-width="100%"></div>
--- wp-content/themes/canvas-child/templates/module-facebook-moderation.php
+++ wp-content/themes/canvas-child/templates/module-facebook-moderation.php
@@ -1,16 +1,18 @@
 <?php defined( 'ABSPATH' ) || die();
 /**
- * Facebook moderation module.
- *
- * @author   Aleksandr Levashov
- * @version  1.0.0
- */
-
+* Facebook moderation module.
+*
+* @author   Aleksandr Levashov
+* @version  1.0.0
+*/
 $thumb = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' );
 ?>
-<meta property="og:title"       content="<?php echo esc_attr( get_the_title() ); ?>" />
-<meta property="og:type"        content="website" />
-<meta property="og:url"         content="<?php echo esc_attr( get_permalink() ); ?>" />
-<meta property="og:image"       content="<?php echo esc_attr( $thumb['0'] ); ?>" />
+<meta property="og:title"       content="<?php echo esc_attr( get_the_title() ); ?>" />
+<meta property="og:type"        content="website" />
+<meta property="og:url"         content="<?php echo esc_attr( str_replace( 'https://', 'http://', get_permalink() ) );
+?>" />
+<meta property="og:image"       content="<?php echo esc_attr( $thumb['0'] ); ?>" />
 <meta property="og:description" content="<?php echo esc_attr( get_the_excerpt() ); ?>" />
-<meta property="fb:app_id"      content="1613892592185308" />
+<meta property="fb:app_id"      content="1613892592185308" />
+
+
