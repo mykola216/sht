a4cfd591b9bb5a716fde8b3ee8dc5d586750cb1f
Maksim Petrenko <maksim.p@smart-gamma.com>
2017-02-10 13:05:14 +0200

    Hot fix https to http for sharethis likes (google, pintirest, twitter);

--- wp-content/themes/canvas-child/templates/module-sharethis.php
+++ wp-content/themes/canvas-child/templates/module-sharethis.php
@@ -5,18 +5,20 @@
  * @author   Aleksandr Levashov
  * @version  1.0.0
  */
+
+$site_url = str_replace( 'https://', 'http://', site_url() );
 ?>
 <div id="social-media">
 	<?php
 	if ( is_front_page() ) {
-		echo '<span class="st_facebook_hcount" displayText="Facebook" st_url="' . esc_attr( str_replace( 'https://', 'http://', site_url() ) ) .'" st_title="' . esc_attr( get_the_title() ) . '"></span>';
+		echo '<span class="st_facebook_hcount" displayText="Facebook" st_url="' . esc_attr( $site_url ) .'" st_title="' . esc_attr( get_the_title() ) . '"></span>';
 	} else {
 		echo '<div class="fb-like" data-href="' . esc_attr( str_replace( 'https://', 'http://', get_permalink() ) ) .'" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>';
 	}
 	?>
-	<span class='st_pinterest_hcount' displayText='Pinterest'></span>
-	<span class='st_twitter_hcount' displayText='Tweet'></span>
-	<span class='st_googleplus_hcount' displayText='Google +'></span>
+	<span class='st_pinterest_hcount' displayText='Pinterest' st_url="<?php echo esc_attr( $site_url ); ?>" st_title="<?php echo esc_attr( get_the_title() ); ?>"></span>
+	<span class='st_twitter_hcount' displayText='Tweet' st_url="<?php echo esc_attr( $site_url ); ?>" st_title="<?php echo esc_attr( get_the_title() ); ?>"></span>
+	<span class='st_googleplus_hcount' displayText='Google +' st_url="<?php echo esc_attr( $site_url ); ?>" st_title="<?php echo esc_attr( get_the_title() ); ?>"></span>
 </div>
 <script type="text/javascript">
 	if (typeof stLight != 'undefined') {
