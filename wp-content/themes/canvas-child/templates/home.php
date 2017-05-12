1673b2eaf057800f1af52027651c8fa91781a184
yurii.s_uzh <yurii.s@smart-gamma.com>
2017-05-12 11:30:00 +0300

    Some change https://crm.smart-gamma.com/issues/2430

--- wp-content/themes/canvas-child/templates/home.php
+++ wp-content/themes/canvas-child/templates/home.php
@@ -53,57 +53,6 @@ $img_our_team = get_field('our_team_home', 'option');
 </div>
 
 
-<div class="type-furniture">
-    <div class="container">
-        <div class="row">
-            <?php
-
-            $counter = 0;
-
-            if( have_rows('kinds_of_furniture_repeater', 'option') ): ?>
-
-                <?php while ( have_rows('kinds_of_furniture_repeater', 'option') ) : the_row();
-                    $image = get_sub_field('img');
-                    $link = get_sub_field('link');
-                    ?>
-                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 wrap-item">
-                        <a href="<?php echo $link; ?>">
-                            <img src="<?php echo $image['url']; ?>" alt="<? echo $image['alt']; ?>">
-                        </a>
-                    </div>
-
-
-                <?php endwhile; ?>
-
-            <?php else :
-
-                // no rows found!!
-                ?>
-
-            <?php endif; ?>
-        </div>
-        <hr>
-    </div>
-</div>
-
-
-<section class="our-team">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 wrap-item">
-                <div class="wrapper">
-                    <?php echo $text_our_team; ?>
-                </div>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 wrap-item">
-                <div class="wrapper">
-                    <?php echo $img_our_team; ?>
-                    <div class="clearfix"></div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
 
 
 
@@ -180,6 +129,68 @@ canvas_child_top_products( array(
 		))
 	));
 ?>
+
+    <div class="type-furniture">
+        <div class="container">
+
+            <?php
+
+            $counter = 0;
+
+            if( have_rows('kinds_of_furniture_repeater', 'option') ): ?>
+
+                <hr>
+
+                <div class="row">
+
+                    <?php while ( have_rows('kinds_of_furniture_repeater', 'option') ) : the_row();
+                        $image = get_sub_field('img');
+                        $link = get_sub_field('link');
+                        ?>
+                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 wrap-item clearfix">
+                            <a href="<?php echo $link; ?>">
+                                <img src="<?php echo $image['url']; ?>" alt="<? echo $image['alt']; ?>">
+                            </a>
+                        </div>
+
+
+                    <?php endwhile; ?>
+
+                </div>
+
+                <hr>
+            <?php else :
+
+                // no rows found!!
+                ?>
+
+            <?php endif; ?>
+
+        </div>
+    </div>
+
+
+    <section class="our-team">
+        <?php if( !have_rows('kinds_of_furniture_repeater', 'option') ): ?>
+            <hr>
+        <?php endif;?>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 wrap-item">
+                    <div class="wrapper">
+                        <?php echo $text_our_team; ?>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 wrap-item">
+                    <div class="wrapper">
+                        <?php echo $img_our_team; ?>
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
 
 
 <!--Band module-->
